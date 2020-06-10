80b1a530a514b1bb06a5ad880b495b3a96608058
Jean-archibald <jvjlondon@outlook.com>
2020-06-10 23:08:14 +0100

    correction of all  depreciations except one about Doctrine/Inflector

--- todoList/config/bundles.php
+++ todoList/config/bundles.php
@@ -9,7 +9,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
---
+++ todoList/tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+
+    public function testHomepageIsUp()
+    {
+        $client = static::createClient();
+        $client->request('GET','/');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+    }
+
+}
